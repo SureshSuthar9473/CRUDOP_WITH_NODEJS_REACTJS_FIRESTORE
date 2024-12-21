e62886dbd23a823e1ee2a53c8d6f141aca0e315a
Suresh Suthar <158549605+suthar-suresh@users.noreply.github.com>
2024-12-21 04:13:57 +0000

    add vercel file

--- Backend/firebase.js
+++ Backend/firebase.js
@@ -1,9 +1,25 @@
 require("dotenv").config();
-const { initializeApp, applicationDefault } = require("firebase-admin/app");
+const { initializeApp, cert } = require("firebase-admin/app");
 const { getFirestore } = require("firebase-admin/firestore");
 
+// Manually providing the Firebase service account credentials
+const serviceAccount = {
+  "type": process.env.TYPE,
+  "project_id":  process.env.PROJECT_ID,
+  "private_key_id": process.env.PRIVATE_KEY_ID,
+  "private_key": process.env.PRIVATE_KEY,
+  "client_email": process.env.CLIENT_EMAIL,
+  "client_id":  process.env.CLIENT_ID,
+  "auth_uri":  process.env.AUTH_URL,
+  "token_uri":  process.env.TOKEN_URI,
+  "auth_provider_x509_cert_url":  process.env.AUTH_PROVIDER_X509_CERT_URL,
+  "client_x509_cert_url":  process.env.CLIENT_X509_CERT_URL,
+  "universe_domain": process.env.UNIVERSE_DOMAIN
+};
+
+// Initialize Firebase Admin SDK with manually provided credentials
 initializeApp({
-  credential: applicationDefault(),
+  credential: cert(serviceAccount),
 });
 
 const db = getFirestore();
